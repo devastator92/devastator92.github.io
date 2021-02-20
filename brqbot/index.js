b2a732359d0d455ea00305c5ee8a00060d3a58e4
Ariel Conejeros <ariel.conejeros.munoz@ciisa.cl>
2021-02-20 19:13:16 -0300

    Update index.js

--- brqbot/index.js
+++ brqbot/index.js
@@ -1,4 +1,4 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.login('Nzc3OTM4MzcxNTgyMzYxNjAw.X7KtkQ._br9M0JY7JIWHKS1ogQJ7_B-Oyc');
+client.login('Nzc3OTM4MzcxNTgyMzYxNjAw.X7KtkQ.pD3YJYM3gGpjYZLkW5B8s9xhfls');
